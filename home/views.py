97a4bb72964e0472962276b13024158beaa5dff2
purnapanigrahy <purnapanigrahy531@gmail.com>
2026-04-15 22:22:03 +0530

    handling error and changes in views.py

--- home/views.py
+++ home/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         userormail = request.POST.get('userormail')
         password = request.POST.get('password')
-        return redirect('profile')
+        user  = authenticate(request,username=userormail,password = password)
+        if user:
+            login(request,user)
+            return render(request,'profile.html')
+        else:
+            error = "incorrect username or password"
+            return render(request,'home.html',{"error":error})
     return render(request,'home.html')
 
 def signup(request):
@@ -15,6 +22,10 @@ def signup(request):
         password = request.POST.get('password')
         cnf_password = request.POST.get('cnf_password')
         email = request.POST.get('email')
+        if User.objects.filter(username=username).exists():
+            error = 'the user alreadey existed.'
+            context = {'error':error}
+            return render(request,'signup.html',context)
         if password==cnf_password:
             user = User.objects.create_user(username=username,email=email,password=password)
         else:
